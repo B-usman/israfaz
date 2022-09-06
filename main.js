a48d69fa89247bc690cf21bcb2df1e926daf98a0
B-usman <babatundeusman82@gmail.com>
2022-09-06 09:11:21 +0100

    website launched

---
+++ main.js
@@ -0,0 +1,147 @@
+
+
+// smart watches
+document.getElementById('smb').onclick = function() {
+    document.getElementById('hidd').style.display = "block";
+    document.getElementById('smc').style.display = "none";
+}
+
+document.getElementById('slb').onclick = function(){
+    document.getElementById('hidd').style.display = "none";
+    document.getElementById('smc').style.display = "block";
+}
+
+// refrigerators
+document.getElementById('smb-rf').onclick = function() {
+    document.getElementById('hidd-rf').style.display = "block";
+    document.getElementById('smc-rf').style.display = "none";
+}
+
+document.getElementById('slb-rf').onclick = function(){
+    document.getElementById('hidd-rf').style.display = "none";
+    document.getElementById('smc-rf').style.display = "block";
+}
+
+// washing Machines
+document.getElementById('smb-ws').onclick = function() {
+    document.getElementById('hidd-ws').style.display = "block";
+    document.getElementById('smc-ws').style.display = "none";
+}
+
+document.getElementById('slb-ws').onclick = function(){
+    document.getElementById('hidd-ws').style.display = "none";
+    document.getElementById('smc-ws').style.display = "block";
+}
+
+// Blenders
+document.getElementById('smb-bl').onclick = function() {
+    document.getElementById('hidd-bl').style.display = "block";
+    document.getElementById('smc-bl').style.display = "none";
+}
+
+document.getElementById('slb-bl').onclick = function(){
+    document.getElementById('hidd-bl').style.display = "none";
+    document.getElementById('smc-bl').style.display = "block";
+}
+
+// tablets and pcs
+document.getElementById('smb-tb').onclick = function() {
+    document.getElementById('hidd-tb').style.display = "block";
+    document.getElementById('smc-tb').style.display = "none";
+}
+
+document.getElementById('slb-tb').onclick = function(){
+    document.getElementById('hidd-tb').style.display = "none";
+    document.getElementById('smc-tb').style.display = "block";
+}
+
+// Phones
+document.getElementById('smb-ph').onclick = function() {
+    document.getElementById('hidd-ph').style.display = "block";
+    document.getElementById('smc-ph').style.display = "none";
+}
+
+document.getElementById('slb-ph').onclick = function(){
+    document.getElementById('hidd-ph').style.display = "none";
+    document.getElementById('smc-ph').style.display = "block";
+}
+
+// Generators
+document.getElementById('smb-gn').onclick = function() {
+    document.getElementById('hidd-gn').style.display = "block";
+    document.getElementById('smc-gn').style.display = "none";
+}
+
+document.getElementById('slb-gn').onclick = function(){
+    document.getElementById('hidd-gn').style.display = "none";
+    document.getElementById('smc-gn').style.display = "block";
+}
+
+// Accesssories
+document.getElementById('smb-acc').onclick = function() {
+    document.getElementById('hidd-acc').style.display = "block";
+    document.getElementById('smc-acc').style.display = "none";
+}
+
+document.getElementById('slb-acc').onclick = function(){
+    document.getElementById('hidd-acc').style.display = "none";
+    document.getElementById('smc-acc').style.display = "block";
+}
+
+// Fans
+document.getElementById('smb-fan').onclick = function() {
+    document.getElementById('hidd-fan').style.display = "block";
+    document.getElementById('smc-fan').style.display = "none";
+}
+
+document.getElementById('slb-fan').onclick = function(){
+    document.getElementById('hidd-fan').style.display = "none";
+    document.getElementById('smc-fan').style.display = "block";
+}
+
+
+
+// modal editing
+
+let goods = document.getElementById('g-d');
+let price = document.getElementById('pr');
+
+document.getElementById('bt1').onclick = function(){
+    goods.value = document.getElementById('gd1').innerHTML;
+    price.value = document.getElementById('pr1').innerHTML;
+}
+
+document.getElementById('bt2').onclick = function(){
+    goods.value = document.getElementById('gd2').innerHTML;
+    price.value = document.getElementById('pr2').innerHTML;
+}
+
+document.getElementById('bt3').onclick = function(){
+    goods.value = document.getElementById('gd3').innerHTML;
+    price.value = document.getElementById('pr3').innerHTML;
+}
+
+document.getElementById('bt4').onclick = function(){
+    goods.value = document.getElementById('gd4').innerHTML;
+    price.value = document.getElementById('pr4').innerHTML;
+}
+
+document.getElementById('bt5').onclick = function(){
+    goods.value = document.getElementById('gd5').innerHTML;
+    price.value = document.getElementById('pr5').innerHTML;
+}
+
+document.getElementById('bt6').onclick = function(){
+    goods.value = document.getElementById('gd6').innerHTML;
+    price.value = document.getElementById('pr6').innerHTML;
+}
+
+document.getElementById('bt7').onclick = function(){
+    goods.value = document.getElementById('gd7').innerHTML;
+    price.value = document.getElementById('pr7').innerHTML;
+}
+
+document.getElementById('bt8').onclick = function(){
+    goods.value = document.getElementById('gd8').innerHTML;
+    price.value = document.getElementById('pr8').innerHTML;
+}
